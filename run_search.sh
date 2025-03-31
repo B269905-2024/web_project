ccba58089b242b455ff610454de2929a28c8f5b3
“B269905-2024” <s2713107@ed.ac.uk>
2025-03-31 21:46:40 +0100

    now it can fetch A LOT of sequences without breaking

--- run_search.sh
+++ run_search.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
 
-# Get parameters from command line
 JOB_ID=$1
 PROTEIN_FAMILY=$2
 TAXONOMIC_GROUP=$3
 
-# Construct query (properly escape quotes)
-#QUERY="($PROTEIN_FAMILY) AND $TAXONOMIC_GROUP[Organism]"
 QUERY="\"$PROTEIN_FAMILY\" AND \"$TAXONOMIC_GROUP\""
-# Get database credentials from login.php
+
 DB_CREDENTIALS=$(php -r 'require_once "login.php"; echo "$hostname\n$database\n$username\n$password\n";')
 read -r HOSTNAME DATABASE USERNAME PASSWORD <<< "$DB_CREDENTIALS"
 
-# Run the search and fetch sequences
-SEQUENCES=$(/home/s2713107/edirect/esearch -db protein -query "$QUERY" | /home/s2713107/edirect/efetch -format fasta)
+# Get sequence data with error handling
+#SEQUENCES=$(curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=protein&id=$(curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=protein&term=$("$QUERY")&retmax=10" | grep -o '<Id>[0-9]*</Id>' | sed 's/<Id>//g; s/<\/Id>/,/g' | tr -d '\n' | sed 's/,$//')&rettype=fasta&retmode=text")
+SEQUENCES=$(curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=protein&id=$(curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=protein&term=$(echo "$QUERY" | sed 's/ /+/g')&retmax=2" | grep -o '<Id>[0-9]*</Id>' | sed 's/<Id>//g; s/<\/Id>/,/g' | tr -d '\n' | sed 's/,$//')&rettype=fasta&retmode=text")
 
-# Check if we got any sequences
+
 if [ -z "$SEQUENCES" ]; then
     STATUS="failed"
 else
     STATUS="completed"
+    # Escape single quotes for SQL
+    SEQUENCES=$(echo "$SEQUENCES" | sed "s/'/''/g")
 fi
 
-# Connect to MySQL and store results
 mysql -h "$HOSTNAME" -u "$USERNAME" -p"$PASSWORD" "$DATABASE" <<EOF
-UPDATE searches 
-SET 
-    status = '$STATUS', 
-    sequences = $(mysql -h "$HOSTNAME" -u "$USERNAME" -p"$PASSWORD" "$DATABASE" -e "SELECT QUOTE('$SEQUENCES')" | tail -n 1),
-    completed_at = NOW() 
+UPDATE searches
+SET
+    status = '$STATUS',
+    sequences = '$SEQUENCES',
+    completed_at = NOW()
 WHERE job_id = '$JOB_ID';
 EOF
 
